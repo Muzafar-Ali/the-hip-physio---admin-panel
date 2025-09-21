f7af4eac155680ad5e979b359bc3b0c90ec95c31
Muzafar-Ali <84478313+Muzafar-Ali@users.noreply.github.com>
2025-09-21 16:35:23 +0500

    added version 2 in vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,7 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://hip-physio-api.onrender.com/api/:path*",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
-
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         destination:
-//           process.env.NODE_ENV === "development"
-//             ? "http://localhost:4000/api/:path*"
-//             : "https://hip-physio-api.onrender.com/api/:path*",
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
